cc03ee09c0b3b47bb792f7746134f7607ecedad2
Mobolaji <bjkuppy@gmail.com>
2022-03-28 19:40:06 +0100

    gets location from lunching the app

--- climaa/lib/screens/loading_screens.dart
+++ climaa/lib/screens/loading_screens.dart
@@ -8,6 +8,11 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   void getLocation() async {
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
@@ -15,16 +20,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold( );
   }
 }
