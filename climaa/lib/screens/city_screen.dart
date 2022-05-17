0418d634376d29ea8e58526276e7c633db01e5d0
Mobolaji <bjkuppy@gmail.com>
2022-05-17 01:24:56 +0100

    feat: created a text field to input the city name and also fix the lint issue

--- climaa/lib/screens/city_screen.dart
+++ climaa/lib/screens/city_screen.dart
@@ -33,7 +33,15 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(Icons.location_city,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
               FlatButton(
                 onPressed: () {},
